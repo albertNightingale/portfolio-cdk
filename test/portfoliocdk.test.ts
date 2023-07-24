d2c617dc2ee110bf94b659399871168bbee9f771
Albert (jiaming) Liu <70285611+albertNightingale@users.noreply.github.com>
2023-07-24 15:26:55 -0700

    Initial commit

---
+++ test/portfoliocdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Portfoliocdk from '../lib/portfoliocdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/portfoliocdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Portfoliocdk.PortfoliocdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
