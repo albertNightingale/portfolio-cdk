9c438f8de5f6cf1b2a480ec1258dc690ef2fa666
Albert (jiaming) Liu <70285611+albertNightingale@users.noreply.github.com>
2023-07-30 13:25:21 -0700

    add code to set up the cdk

---
+++ build4deploy.sh
@@ -0,0 +1,8 @@
+
+# build the background
+cd ../background
+npm run build
+
+# build the express server
+cd ../backend
+npm run build
--- lib/portfoliocdk-stack.ts
+++ lib/portfoliocdk-stack.ts
@@ -1,16 +1,59 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import * as events from "aws-cdk-lib/aws-events";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class PortfoliocdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // express server lambda, api gateway
+    const expressServerLambda = new lambda.Function(this, "express-server-handler", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, "../../backend/dist/")),
+      handler: "index_prod.handler",
+      environment: {
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PortfoliocdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    expressServerLambda.addToRolePolicy(new PolicyStatement({
+      actions: ["s3:*", "s3-object-lambda:*"],
+      resources: ["arn:aws:s3:::portfolio-bucket-albert"]
+    }));
+
+    const api = new apigateway.RestApi(this, "portfolio-backend", {
+      restApiName: "portfolio-backend",
+      description: "This server respond to http requests form the frontend"
+    });
+
+    const apiGateway = new apigateway.LambdaIntegration(expressServerLambda, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addResource("githubcontribution").addMethod("GET", apiGateway); // GET 
+    api.root.addResource("projects").addMethod("GET", apiGateway); // GET 
+
+    // background lambda, event rule
+    const backgroundLambda = new lambda.Function(this, "background-handler", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, "../../background/dist/")),
+      handler: "index_prod.handler",
+      environment: {
+      }
+    });
+
+    backgroundLambda.addToRolePolicy(new PolicyStatement({
+      actions: ["s3:*", "s3-object-lambda:*"],
+      resources: ["arn:aws:s3:::portfolio-bucket-albert"]
+    }));
+
+    const eventRule = new events.Rule(this, 'background-trigger-timer', {
+      schedule: events.Schedule.cron({ minute: '0', hour: '1' }),
+    });
+    eventRule.addTarget(new targets.LambdaFunction(backgroundLambda))
   }
 }
